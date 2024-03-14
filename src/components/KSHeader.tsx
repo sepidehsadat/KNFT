7b2b5951e3717387bb895c98d8fec0c0658c9f28
Sepideh <ssmazlumi@gmail.com>
2024-03-14 22:50:41 +0330

    header

--- src/components/KSHeader.tsx
+++ src/components/KSHeader.tsx
@@ -11,24 +11,23 @@ export default function KSHeader()
 {
     return (
         <>
-            <Navbar expand="lg" className={`${Style.ks_header} bg-body-tertiary`}>
+            <Navbar expand="lg" className={`${Style.ks_header} bg-body-tertiary `}>
                 <Container>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Brand href="#home"> <img className={Style.ks_header_logo} src={logo} alt="logo" /></Navbar.Brand>
-                    <div className='d-block d-md-none'>
-                        <KSButtonGray title={"ورود و ثبت نام"} />
-                    </div>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#home" className={Style.active}>صفحه اصلی</Nav.Link>
-                            <Nav.Link href="#link"> لیست دوره ها</Nav.Link>
-                            <Nav.Link href="#link">درباره ما</Nav.Link>
-                            <Nav.Link href="#link">ارتباط با ما</Nav.Link>
+                            <Nav.Link href="#home" className="active">Home </Nav.Link>
+                            <Nav.Link href="#link">All NFTS</Nav.Link>
+                            <Nav.Link href="#link">Team</Nav.Link>
+                            <Nav.Link href="#link">Terms & Co</Nav.Link>
+
+                            <Nav.Link href="#link">Contact</Nav.Link>
+
                         </Nav>
                         <div className='d-flex'>
-                            <p className='font-14-normal-400 mb-0 ms-4'>ارتباط با پشتیبانی<br />۰۲۱-۴۵۶۵۶</p>
-                            <KSButton title={"ورود و ثبت نام"} onclick={()=>{}}/>
+                            <KSButton title={"Get in Touch"} onclick={() => { }} />
                         </div>
                     </Navbar.Collapse>
                 </Container>
