c90ff82ef3ecbd5e88d971071016e3108ed91436
Sepideh <ssmazlumi@gmail.com>
2024-09-14 14:04:28 +0330

    Footer

--- src/components/KSHeader.tsx
+++ src/components/KSHeader.tsx
@@ -3,7 +3,7 @@ import Style from "./KSHeader.module.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from "../assets/images/header_logo.png";
+import logo from "../assets/images/logo.png";
 import KSButton from './KSButton'
 
 export default function KSHeader()
